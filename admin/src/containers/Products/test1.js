0dfc7e1941d604c4d004d9435641357f0c982ee9
harshitgupta2019 <63345282+harshitgupta2019@users.noreply.github.com>
2022-07-31 22:41:29 +0530

    Add files via upload

---
+++ admin/src/containers/Products/test1.js
@@ -0,0 +1,66 @@
+import Web3 from "web3";
+import { test } from "./test";
+// import { contact } from "./contact";
+export const loadBlockchain = async () => {//change fn name
+  const web3 = new Web3(window.ethereum);
+  const accounts = await web3.eth.getAccounts();
+  const voting = new web3.eth.Contract(
+    
+  [
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+],
+    "0x54Af8Ff8f2CA66C628AD5DfEa49F55651947bC2b"
+  );
+  return { accounts, voting };//change names
+};
